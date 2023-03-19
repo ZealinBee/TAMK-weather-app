9fe5635730c1c89586d085a3bac8ac890efde30c
Zhiyuan Liu <thenukezealot@gmail.com>
2023-03-20 01:11:17 +0200

    chart implemented

--- js/script.js
+++ js/script.js
@@ -2,9 +2,28 @@ const refreshButton = document.querySelector(".refresh-button");
 const infoPageButton = document.querySelector(".info-page-button");
 const mainTableBody = document.querySelector(".main-table-body");
 const temperatureTableBody = document.querySelector(".temperature-table-body");
+const windSpeedTableBody = document.querySelector(".wind-speed-table-body");
 refreshButton.addEventListener("click", loadMainWeather);
 addEventListener("load", loadMainWeather);
 
+const defaultChartOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    legend: {
+      labels: {
+        font: {
+          family: "'Poppins', 'sans-serif'",
+          size: 14,
+        },
+        color: "white",
+      },
+    },
+  },
+};
 function loadMainWeather() {
   const mainTableBody = document.querySelector(".main-table-body");
 
@@ -29,7 +48,7 @@ function loadMainWeather() {
         }
 
         const tr = `
-            <td>${index}</td>
+            <td class="bold">${index}</td>
             <td class="capitalize">${type}</td>
             <td>${date}</td>
             <td>${time}</td>
@@ -49,27 +68,94 @@ function showPage(page) {
   selectedPage.style.display = "block";
   if (page === "temperature") {
     loadTemperature();
+  } else if (page === "wind-speed") {
+    loadWindSpeed();
   }
 }
 
 function loadTemperature() {
   temperatureTableBody.innerHTML = "";
+  const labels = [];
+  const dataset = [];
+
   fetch("http://webapi19sa-1.course.tamk.cloud/v1/weather/temperature")
     .then((response) => response.json())
     .then((data) => {
       data.map((item, index) => {
         const date = item.date_time.slice(0, 10);
         const time = item.date_time.slice(11, 19);
-        const temperature = Object.values(item.temperature);
+        const temperature = Object.values(item.temperature).join("");
         index += 1;
         if (index < 10) index = `0${index}`;
         const tr = `
-        <td>${index}</td>
+        <td class="bold">${index}</td>
         <td>${date}</td>
         <td>${time}</td>
         <td>${temperature}</td>
         `;
         temperatureTableBody.innerHTML += tr;
+        labels.push(time);
+        dataset.push(temperature);
+      });
+
+      const temperatureChart = document.getElementById("temperature-chart");
+      new Chart(temperatureChart, {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Temperature in Celsius",
+              data: dataset,
+              backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+              borderColor: ["rgba(226, 146, 177, 0.8)"],
+              borderWidth: 2,
+            },
+          ],
+        },
+        options: defaultChartOptions,
       });
     });
+}
+function loadWindSpeed() {
+  windSpeedTableBody.innerHTML = "";
+  const labels = [];
+  const dataset = [];
+  fetch("http://webapi19sa-1.course.tamk.cloud/v1/weather/latest/wind_speed")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item, index) => {
+        const date = item.date_time.slice(0, 10);
+        const time = item.date_time.slice(11, 19);
+        const wind_speed = Object.values(item.wind_speed);
+        index += 1;
+        if (index < 10) index = `0${index}`;
+        const tr = `
+        <td class="bold">${index}</td>
+        <td>${date}</td>
+        <td>${time}</td>
+        <td>${wind_speed}</td>
+        `;
+        windSpeedTableBody.innerHTML += tr;
+        labels.push(time);
+        dataset.push(wind_speed);
+      });
+    });
+    const windSpeedChart = document.getElementById("wind-speed-chart");
+      new Chart(windSpeedChart, {
+        type: "bar",
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: "Wind Speed",
+              data: dataset,
+              backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+              borderColor: ["rgba(226, 146, 177, 0.8)"],
+              borderWidth: 2,
+            },
+          ],
+        },
+        options: defaultChartOptions,
+      });
 }

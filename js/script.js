ecb0468122dabaee64a881f41fdb25e9a31b4257
Zhiyuan Liu <thenukezealot@gmail.com>
2023-03-21 08:51:21 +0200

    others page implemented

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ const infoPageButton = document.querySelector(".info-page-button");
 const mainTableBody = document.querySelector(".main-table-body");
 const temperatureTableBody = document.querySelector(".temperature-table-body");
 const windSpeedTableBody = document.querySelector(".wind-speed-table-body");
+const rainTableBody = document.querySelector(".rain-table-body");
+const windDirectionTableBody = document.querySelector(".wind-direction-table-body");
+const lightTableBody = document.querySelector(".light-table-body");
 refreshButton.addEventListener("click", loadMainWeather);
 addEventListener("load", loadMainWeather);
 
@@ -10,6 +13,14 @@ const defaultChartOptions = {
   scales: {
     y: {
       beginAtZero: true,
+      ticks: {
+        color: "#FFF",
+      },
+    },
+    x: {
+      ticks: {
+        color: "#FFF",
+      },
     },
   },
   plugins: {
@@ -70,6 +81,12 @@ function showPage(page) {
     loadTemperature();
   } else if (page === "wind-speed") {
     loadWindSpeed();
+  } else if (page === "rain") {
+    loadRain();
+  } else if(page === "wind-direction"){
+    loadWindDirection();
+  } else if(page === "light") {
+    loadLight()
   }
 }
 
@@ -141,21 +158,147 @@ function loadWindSpeed() {
         dataset.push(wind_speed);
       });
     });
-    const windSpeedChart = document.getElementById("wind-speed-chart");
-      new Chart(windSpeedChart, {
-        type: "bar",
-        data: {
-          labels: labels,
-          datasets: [
-            {
-              label: "Wind Speed",
-              data: dataset,
-              backgroundColor: ["rgba(226, 146, 177, 0.5)"],
-              borderColor: ["rgba(226, 146, 177, 0.8)"],
-              borderWidth: 2,
-            },
-          ],
+  const windSpeedChart = document.getElementById("wind-speed-chart");
+  new Chart(windSpeedChart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Wind Speed",
+          data: dataset,
+          backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+          borderColor: ["rgba(226, 146, 177, 0.8)"],
+          borderWidth: 2,
         },
-        options: defaultChartOptions,
+      ],
+    },
+    options: defaultChartOptions,
+  });
+}
+function loadRain() {
+  rainTableBody.innerHTML = "";
+  const labels = [];
+  const dataset = [];
+  fetch("https://webapi19sa-1.course.tamk.cloud/v1/weather/rain")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item, index) => {
+        const date = item.date_time.slice(0, 10);
+        const time = item.date_time.slice(11, 19);
+        const rain = Object.values(item.rain).join("");
+        index += 1;
+        if (index < 10) index = `0${index}`;
+        const tr = `
+        <td class="bold">${index}</td>
+        <td>${date}</td>
+        <td>${time}</td>
+        <td>${rain}</td>
+        `;
+        rainTableBody.innerHTML += tr;
+        labels.push(time);
+        dataset.push(rain);
       });
+    });
+  const rainChart = document.getElementById("rain-chart");
+  new Chart(rainChart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Rain",
+          data: dataset,
+          backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+          borderColor: ["rgba(226, 146, 177, 0.8)"],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: defaultChartOptions,
+  });
+}
+async function loadWindDirection() {
+  windDirectionTableBody.innerHTML = "";
+  const labels = [];
+  const dataset = [];
+  await fetch("https://webapi19sa-1.course.tamk.cloud/v1/weather/wind_direction")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item, index) => {
+        const date = item.date_time.slice(0, 10);
+        const time = item.date_time.slice(11, 19);
+        const wind_direction = Object.values(item.wind_direction).join("");
+        index += 1;
+        if (index < 10) index = `0${index}`;
+        const tr = `
+        <td class="bold">${index}</td>
+        <td>${date}</td>
+        <td>${time}</td>
+        <td>${wind_direction}</td>
+        `;
+        windDirectionTableBody.innerHTML += tr;
+        labels.push(time);
+        dataset.push(wind_direction);
+      });
+    });
+  const windDirectionChart = document.getElementById("wind-direction-chart");
+  new Chart(windDirectionChart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Wind Direction",
+          data: dataset,
+          backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+          borderColor: ["rgba(226, 146, 177, 0.8)"],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: defaultChartOptions,
+  });
+}
+function loadLight() {
+  lightTableBody.innerHTML = "";
+  const labels = [];
+  const dataset = [];
+  fetch("https://webapi19sa-1.course.tamk.cloud/v1/weather/light")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item, index) => {
+        const date = item.date_time.slice(0, 10);
+        const time = item.date_time.slice(11, 19);
+        const light = Object.values(item.light).join("");
+        index += 1;
+        if (index < 10) index = `0${index}`;
+        const tr = `
+        <td class="bold">${index}</td>
+        <td>${date}</td>
+        <td>${time}</td>
+        <td>${light}</td>
+        `;
+        lightTableBody.innerHTML += tr;
+        labels.push(time);
+        dataset.push(light);
+      });
+    });
+  const lightChart = document.getElementById("light-chart");
+  new Chart(lightChart, {
+    type: "bar",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Light",
+          data: dataset,
+          backgroundColor: ["rgba(226, 146, 177, 0.5)"],
+          borderColor: ["rgba(226, 146, 177, 0.8)"],
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: defaultChartOptions,
+  });
 }
